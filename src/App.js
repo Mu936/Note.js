bcd2013e17a05080b89f88dceb58795cd2753ccd
Mu936 <masithembimulaloivy@gmail.com>
2024-10-09 14:22:16 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import NotesList from './components/NotesList';
+import NoteEditor from './components/NoteEditor';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={NotesList} />
+        <Route path="/note/:id" component={NoteEditor} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
